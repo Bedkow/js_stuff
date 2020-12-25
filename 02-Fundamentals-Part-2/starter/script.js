a7d69d4bcb39491a471c91d1d2696ce2ac4a27ba
Bedkow <bednarekdaniel@o2.pl>
2020-12-25 19:18:09 +0100

    2nd commit

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// function logger() {
+//     console.log("nibba");
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = "Juice with " + apples + " apples and" + oranges + " oranges"
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
