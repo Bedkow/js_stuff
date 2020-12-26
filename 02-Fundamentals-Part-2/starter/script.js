a15ecaa279b0d0ad377e9e6e3399a752ad4aa83a
Bedkow <bednarekdaniel@o2.pl>
2020-12-26 01:31:41 +0100

    3rd

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -17,3 +17,57 @@
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+// function calcAge1(birthYear) {
+//     const age = 2020 - birthYear;
+//     return age;
+// }
+
+// function calcAge1(birthYear) {
+//     return 2020 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+
+// const calcAge2 = function (birthYear) {
+//     return 2020 - birthYear;
+// }
+
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// const calcAge3 = birthYear => 2020 - birthYear;
+// const age3 = calcAge3(1991);
+
+// const yearsUntilRetirement = birthyear => {
+//     const age = 2020 - birthyear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+
+// console.log(yearsUntilRetirement)(1991));
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2020 - birthYear;
+//     const retirement = 65 - age;
+//     return "$(firstName) retires in $(retirement) years";
+// }
+
+// console.log(yearsUntilRetirement())
+
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+// }
+
+// fruitProcessor(2, 3);
+
