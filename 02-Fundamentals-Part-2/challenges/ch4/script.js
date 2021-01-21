6e50fb437865046dba0aa488c4f0874e296df3c0
Bedkow <bednarekdaniel@o2.pl>
2021-01-21 21:13:01 +0100

    completed fundamentals pt 1 and 2

---
+++ 02-Fundamentals-Part-2/challenges/ch4/script.js
@@ -0,0 +1,24 @@
+var bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+var tips = [];
+var totals = [];
+
+function calcTip(billValue) {
+    if (billValue >= 50 && billValue <= 300) {
+        var tip = billValue * 0.15;
+    } else {
+        var tip = billValue * 0.20;
+    }
+    return tip
+}
+
+var tip;
+
+for (x = 0; x <= bills.length - 1; x++) {
+    tip = calcTip(bills[x]);
+    tips.push(tip);
+    totals.push(bills[x] + tip)
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
