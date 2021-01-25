042da44684777dc7010a8d3f3b3e9976ea5a918a
Bedkow <bednarekdaniel@o2.pl>
2021-01-25 21:59:26 +0100

    starting DOM

--- 02-Fundamentals-Part-2/challenges/ch4/script.js
+++ 02-Fundamentals-Part-2/challenges/ch4/script.js
@@ -3,22 +3,23 @@ var tips = [];
 var totals = [];
 
 function calcTip(billValue) {
-    if (billValue >= 50 && billValue <= 300) {
-        var tip = billValue * 0.15;
-    } else {
-        var tip = billValue * 0.20;
-    }
-    return tip
+  if (billValue >= 50 && billValue <= 300) {
+    var tip = billValue * 0.15;
+  } else {
+    var tip = billValue * 0.2;
+  }
+  return tip;
 }
 
 var tip;
 
 for (x = 0; x <= bills.length - 1; x++) {
-    tip = calcTip(bills[x]);
-    tips.push(tip);
-    totals.push(bills[x] + tip)
+  tip = calcTip(bills[x]);
+  tips.push(tip);
+  totals.push(bills[x] + tip);
 }
 
 console.log(bills);
 console.log(tips);
 console.log(totals);
+
