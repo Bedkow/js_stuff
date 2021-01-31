9f488d5e097f5370dff6c4cdbfd3cb6784a1435e
Bedkow <bednarekdaniel@o2.pl>
2021-01-31 20:20:41 +0100

    finished the game project in DOM

--- 03-DOM and Events/script.js
+++ 03-DOM and Events/script.js
@@ -11,11 +11,64 @@
 // document.querySelector('.guess').value = 23
 // console.log(document.querySelector('.guess').value);
 
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function() {
     let guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
+
+    // when no input
     if(!guess) {
         document.querySelector('.message').textContent = 'No number!';
+
+    // when player wins
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber; 
+        // CSS manipulation
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+        // highscore?
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    // when number too high
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+
+        // when number too low
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
+});
+
+// challenge 1: program the "Again!" button
+
+document.querySelector('.again').addEventListener('click', function() {
+    document.querySelector('body').style.backgroundColor = '#222';
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    score = 20; 
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = '';
 });
