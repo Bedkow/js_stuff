cd654de5b02b7cbea3869bfb5215e5f398eab862
Bedkow <bednarekdaniel@o2.pl>
2021-03-09 21:54:40 +0100

    destr objects

--- 06 - data structures modern-operators strings/script.js
+++ 06 - data structures modern-operators strings/script.js
@@ -6,19 +6,33 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, //24 h
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  }
-
+  },
 };
-
-const arr = [2,3,4];
+/*
+//@@@@@@@@@@@@@@@ Arrays @@@@@@@@@@@@@@//
+const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-const [x,y,z] = arr; // original array not affected
+const [x, y, z] = arr; // original array not affected
 console.log(x, y, z);
 console.log(arr);
 
@@ -27,7 +41,7 @@ console.log(arr);
 let [main, , secondary] = restaurant.categories; // 2nd element gets omitted
 
 [main, secondary] = [secondary, main];
-console.log(main, secondary)
+console.log(main, secondary);
 
 // receive 2 return values from func
 const [starter, mainCourse] = restaurant.order(2, 0);
@@ -45,3 +59,16 @@ console.log(i, j, k);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r); // the default value (1) is left for 'r'
+*/
+//@@@@@@@@@@@@ Objects @@@@@@@@@@@@@@//
+
+const { name, openingHours, categories } = restaurant; // order in objs doesnt matter so no need to "skip" like in arr
+console.log(name, openingHours, categories);
+
+const { //destruct + giving different variable names
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags 
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
