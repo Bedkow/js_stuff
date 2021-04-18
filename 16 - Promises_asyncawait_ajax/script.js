9a1704ee3a6776e130aa6e260462156c7d96cea2
Bedkow <bednarekdaniel@o2.pl>
2021-04-18 17:26:39 +0200

    welcome to callback hell

--- 16 - Promises_asyncawait_ajax/script.js
+++ 16 - Promises_asyncawait_ajax/script.js
@@ -2,5 +2,95 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+/*
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest(); // the old way of ajax
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); // GET to get data, + url for Portugal
+
+  // CORS - cross origin resource sharing - without it we cannot access a 3rd party API from our code
+
+  request.send(); // sending the request to the API, canot assign a variable to it as it doesn't have any data yet (async), when the fetching is done it will emit the 'load' event so we can add an EventListener
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText) // 'this' = 'request' object
+    //responseText property stores the response from API >>>
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+*/
+
+const renderCountry = function (data) {
+  const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+
+  //AJAX call country 1
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+
+    //render country 1
+    renderCountry(data);
+
+    // get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    //AJAX call country 2
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function() {
+      const data2 = JSON.parse(this.responseText);
+      console.log(this.responseText);
+    })
+  });
+};
+
+getCountryAndNeighbour('portugal');
